bac1aa066ff5e308f19bbe9c276b4fe88d923f6a
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-11-06 19:51:41 +0530

    added edge cases

--- App.js
+++ App.js
@@ -4,6 +4,11 @@ const qrImg = document.getElementById("img");
 
 button.addEventListener("click", () => {
   const inputValue = input.value;
-  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${inputValue}`;
-  qrImg.style.display = "block";
+  if (inputValue) {
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${inputValue}`;
+    qrImg.style.display = "block";
+  } else {
+    qrImg.style.display = "none";
+    alert("Please put some text in the below field");
+  }
 });
